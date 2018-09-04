2b8020204c8940865e6e45e2fbbdaaef4777ce1f
Jack <jackmeyer@live.com.au>
2018-09-04 10:47:57 +1000

    fixedCode

--- mergeCase1.js
+++ mergeCase1.js
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 var firstName = "";
-=======
-var firstName = "Phil";
->>>>>>> origin/ChangeNamePhil
 var lastName = "";
 
 var fullName = (firstName + lastName);
-=======
-    var firstName = "";
-    var lastName = "Collins";
-
-    var fullName = (firstName + lastName);
->>>>>>> origin/ChangeNameCollins
